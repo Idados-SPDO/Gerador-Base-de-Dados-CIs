f71081284db7ad5f6b6c465d551daa35c04e7dfd
Hugo Monteiro de Souza <hugo.monteiro@fgv.br>
2024-05-22 10:09:24 -0300

    app: permitir que o usuario selecione o local de download da nova base

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import pandas as pd
 from datetime import datetime as dt
-import os
-import time
+import io
 
 def gerar_base():
     dia_atual = dt.today().strftime('%d')  # Retorna o dia atual
@@ -11,35 +10,40 @@ def gerar_base():
 
     arquivo_tratado = str(dia_atual) + '.' + str(numero_mes_atual) + '.' + str(ano_atual) + '.xlsx'
 
-    caminha_salvar = f'W:\\Planejamento e Controle - Coleta de Dados\\PC Construção e Indústria\\1. Painel de Controle SCI\\2. Dados\\{arquivo_tratado}'
-
     for uploaded_file in uploaded_files:
         if uploaded_file.name.startswith('RelatorioCIs_'):
-            progress_bar = st.progress(0)
-            status_text = st.empty()
+            st.markdown(
+                """
+                <style>
+                .stProgress .st-bo {
+                    background-color: orange;
+                }
+                </style>
+                """, 
+            unsafe_allow_html=True
+            )
+            progress_text = "Carregando dados..."
+            progress_bar = st.progress(0, text=progress_text)
             
-            progress_bar.progress(10)
-            status_text.text('Carregando planilhas...')
+            progress_bar.progress(10, text=progress_text)
 
             df_conf_desc = pd.read_excel(uploaded_file, sheet_name='Confirmação de descrição')
-            progress_bar.progress(20)
+            progress_bar.progress(20, text=progress_text)
 
             df_conf_preco = pd.read_excel(uploaded_file, sheet_name='Confirmação do preço')
-            progress_bar.progress(30)
+            progress_bar.progress(30, text=progress_text)
 
             df_agenda = pd.read_excel(uploaded_file, sheet_name='Agendamento de reunião com resp')
-            progress_bar.progress(40)
+            progress_bar.progress(40, text=progress_text)
 
             df_amp = pd.read_excel(uploaded_file, sheet_name='Ampliação de coleta de preços')
-            progress_bar.progress(50)
+            progress_bar.progress(50, text=progress_text)
 
             df_imp = pd.read_excel(uploaded_file, sheet_name='Implantação de coleta de preço')
-            progress_bar.progress(60)
+            progress_bar.progress(60, text=progress_text)
 
             df_carga = pd.read_excel(uploaded_file, sheet_name='Carga de preço')
-            progress_bar.progress(70)
-
-            status_text.text('Processando dados...')
+            progress_bar.progress(70, text=progress_text)
 
             df_conf_desc['Tipo CI'] = 'Confirmação'
             df_conf_preco['Tipo CI'] = 'Confirmação'
@@ -54,12 +58,24 @@ def gerar_base():
             df_carga = df_carga.rename(columns={'Insumo Informado': 'Insumo informado', 'Cód. informante': 'Cód. Informante'})
 
             df_base = pd.concat([df_conf_desc, df_conf_preco, df_agenda, df_amp, df_imp, df_carga], ignore_index=True)
-            progress_bar.progress(90)
+            progress_bar.progress(90, text=progress_text)
 
-            df_base.to_excel(caminha_salvar, index=False)
+            # Salva o dataframe em um buffer de memória
+            buffer = io.BytesIO()
+            df_base.to_excel(buffer, index=False)
+            buffer.seek(0)
+
             progress_bar.progress(100)
 
-            st.success(f'A nova base foi gerada e salva com sucesso!')
+            st.success(f'A nova base foi gerada com sucesso!')
+
+            # Adiciona o botão de download
+            st.download_button(
+                label='Baixar nova base',
+                data=buffer,
+                file_name=arquivo_tratado,
+                mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+            )
         else:
             st.error('Nome de arquivo inválido. O arquivo selecionado deve ser iniciado em: "RelatorioCIs_"')
 
@@ -71,7 +87,6 @@ st.set_page_config(
 )
 
 st.title('Gerador de Bases de Dados para o :orange[Painel de Controle de CIs]')
-
 
 st.write('')
 st.write('')
@@ -92,5 +107,4 @@ container.caption('Pressione o botão abaixo parar gerar a nova base')
 botao = container.button('Gerar nova base')
 
 if botao:
-    with st.spinner('Aguarde enquanto a base é gerada...'):
-        gerar_base()
+    gerar_base()
